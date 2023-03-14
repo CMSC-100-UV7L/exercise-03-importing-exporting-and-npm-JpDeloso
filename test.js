fe5157cf3dc9ffa01de483fe9d5b1c7770bf507d
JpDeloso <jndeloso@up.edu.ph>
2023-03-14 16:31:51 +0800

    feat: added test cases for the 1st function

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
-import addAccount from "./functions.js";
+import functions from "./functions.js";
 
-//Test cases
-addAccount(["Alan", "Turing", "aturing@w3c.com", 58]); // successful
-addAccount(["France", "Castro", "fcastro@w3c.com", "58"]); // wrong data type for the age
-addAccount(["Alan", "Turing", "aturing@w3c.com", 17]); // age is below 18
-addAccount(["Alan", "", "aturing@w3c.com", 58]); // has an empty string
-addAccount(["Alan", "Turing", "aturing@w3c.com"]); // not all fields are present
-addAccount(["Alan", "Turing", "aturing@com", 58]); // invalid email format
-addAccount(["Ted", "Nelson", "ted.n@w3c.com", 43]); // successful
+//Test cases for the 1st function
+console.log(functions.generateUniqueID("Alan","Turing"));
+console.log(functions.generateUniqueID("Ted","Nelson"));
+
+//Test cases for the 2nd function
+functions.addAccount(["Alan", "Turing", "aturing@w3c.com", 58]); // successful
+functions.addAccount(["France", "Castro", "fcastro@w3c.com", "58"]); // wrong data type for the age
+functions.addAccount(["Alan", "Turing", "aturing@w3c.com", 17]); // age is below 18
+functions.addAccount(["Alan", "", "aturing@w3c.com", 58]); // has an empty string
+functions.addAccount(["Alan", "Turing", "aturing@w3c.com"]); // not all fields are present
+functions.addAccount(["Alan", "Turing", "aturing@com", 58]); // invalid email format
+functions.addAccount(["Ted", "Nelson", "ted.n@w3c.com", 43]); // successful

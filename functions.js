f62e01628bf2b7662cb78b0d809988b35d4c0e2e
JpDeloso <jndeloso@up.edu.ph>
2023-03-14 11:27:19 +0800

    feat: created the generateUniqueID function

--- functions.js
+++ functions.js
@@ -1,1 +1,14 @@
-console.log("HELLO WORLD")
+//Author: Jeanfer Phi N. Deloso
+//Student Number: 2021-01227
+//Section: CMSC 100 - UV7L
+//Date: March 14, 2023
+
+import { v4 as uuid } from 'uuid';
+
+function generateUniqueID(firstName, lastName){
+    var uniqueID = firstName.charAt(0).toLowerCase()
+    uniqueID += lastName.toLowerCase() + uuid().slice(0,8) ;
+    return uniqueID;
+}
+
+console.log(generateUniqueID("Allan", "Turing"));

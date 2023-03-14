be8bc1fdc564bd795e1fb3358cd72a2ba6ffb8fb
JpDeloso <jndeloso@up.edu.ph>
2023-03-14 16:12:31 +0800

    chore: added test cases and documentation

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 import addAccount from "./functions.js";
+
+//Test cases
+addAccount(["Alan", "Turing", "aturing@w3c.com", 58]); // successful
+addAccount(["France", "Castro", "fcastro@w3c.com", "58"]); // wrong data type for the age
+addAccount(["Alan", "Turing", "aturing@w3c.com", 17]); // age is below 18
+addAccount(["Alan", "", "aturing@w3c.com", 58]); // has an empty string
+addAccount(["Alan", "Turing", "aturing@w3c.com"]); // not all fields are present
+addAccount(["Alan", "Turing", "aturing@com", 58]); // invalid email format
+addAccount(["Ted", "Nelson", "ted.n@w3c.com", 43]); // successful
